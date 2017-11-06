64802951720a29917838a1e679687455977cac72
CMshadow <bjwmc1996@gmail.com>
2017-11-05 17:01:53 -0800

    map

---
+++ examples/styles.js
@@ -0,0 +1,29 @@
+var styles = [
+  {
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  },{
+    "featureType": "poi",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "administrative",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "featureType": "administrative.country",
+    "elementType": "geometry",
+    "stylers": [
+      { "visibility": "on" }
+    ]
+  }
+];
